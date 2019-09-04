4566f9307e9f7dd8456520f13142d3615a701547
Daniel Lechok <dclechok@gmail.com>
2019-09-04 19:11:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,9 @@ def reverse_each_word (a_string)
   new_array = []
   i = 0
   a_string.each do |word|
-    new_array[0] = word
+    new_array[i] = word
+    i + = 1
+    
   end
   
   return new_array
