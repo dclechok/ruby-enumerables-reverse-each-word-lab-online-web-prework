1655612b17cc84017a4d13fdc4d3be90d6632481
Daniel Lechok <dclechok@gmail.com>
2019-09-04 19:57:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,17 @@
-def reverse_each_word (a_string)
-	rev_array = a_string.chars
-	new_array = []
+# def reverse_each_word (a_string)
+# 	rev_array = a_string.chars
+# 	new_array = []
 	
-	rev_array.each do |s|  
-		new_array.unshift(s)	
-	end
-	new_string = new_array.join()
-	set_words_array = new_string.split
-	final_array = []
-	set_words_array.each do |word|
-		final_array.unshift(word)
-	end
+# 	rev_array.each do |s|  
+# 		new_array.unshift(s)	
+# 	end
+# 	new_string = new_array.join()
+# 	set_words_array = new_string.split
+# 	final_array = []
+# 	set_words_array.each do |word|
+# 		final_array.unshift(word)
+# 	end
 
-	p final_array.join(" ")
+# 	p final_array.join(" ")
 	
-end
+# end
