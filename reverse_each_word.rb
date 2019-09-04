3437af5eca9f8aec1d3ec807f8c781b576a3cf1e
Daniel Lechok <dclechok@gmail.com>
2019-09-04 19:28:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word (a_string)
-  new_array = []
-  i = 0
-  a_string.each do |word|
-    new_array.shift
-    
-  end
-  
-  return new_array
+	arr_w_string = a_string.split
+	new_array = []
+
+	arr_w_string.each do |s|  
+		new_array.unshift(s)
+	end
+
+  return new_array.join(" ")
 end
